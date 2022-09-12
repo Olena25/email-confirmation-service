70d290e3b8799df62535a14eef029e3e50826cb9
olena <olena.vitiuk123@gmail.com>
2022-09-12 10:06:07 +0200

    JLBO-48 Add two basic API Send email to the user with confirmation code and verify this confirmation code for user

---
+++ src/main/java/com/intellias/emailsender/dto/ConfirmationSuccessResponse.java
@@ -0,0 +1,12 @@
+package com.intellias.emailsender.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ConfirmationSuccessResponse {
+    private String message;
+}
